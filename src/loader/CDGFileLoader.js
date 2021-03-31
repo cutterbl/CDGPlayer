04c65a803aad2590ad41fbb38418eaf529798245
Blades, Stephen <Stephen.Blades@ul.com>
2021-03-31 16:54:01 -0500

    chore: Update dependencies

--- src/loader/CDGFileLoader.js
+++ src/loader/CDGFileLoader.js
@@ -4,7 +4,7 @@
 import Deferred from '../utilities/deferred.js';
 // import jsmediatags from 'jsmediatags';
 
-const getDataFile = function(filePath) {
+const getDataFile = function (filePath) {
   const deferred = new Deferred();
   JSZipUtils.getBinaryContent(filePath, (err, data) => {
     if (err) {
@@ -16,19 +16,19 @@ const getDataFile = function(filePath) {
   return deferred.promise;
 };
 
-const loadZipBuffer = function(fileBuffer) {
-  return JSZip.loadAsync(fileBuffer).catch(error =>
+const loadZipBuffer = function (fileBuffer) {
+  return JSZip.loadAsync(fileBuffer).catch((error) =>
     Promise.reject(new Error(`There was an error reading the zip file.`, error))
   );
 };
 
-const loadAudio = function(zipEntry) {
+const loadAudio = function (zipEntry) {
   return zipEntry
     .async('arraybuffer')
     .catch(() => Promise.reject(new Error(`Unable to load the audio file`)));
 };
 
-const getFallbackTagData = function(name) {
+const getFallbackTagData = function (name) {
   const parts = name.split(' - ');
   const tag = { tags: {} };
   // most downloaded cdgs have filenames 'album - artist - songtitle
@@ -38,48 +38,46 @@ const getFallbackTagData = function(name) {
   return tag;
 };
 
-const getTagData = function(zipEntry) {
+const getTagData = function (zipEntry) {
   return zipEntry
     .async('blob')
     .then(
-      buffer =>
-        new Promise(resolve => {
+      (buffer) =>
+        new Promise((resolve) => {
           new jsmediatags.Reader(buffer)
             .setTagsToRead(['title', 'artist', 'album', 'track', 'year', 'genre', 'picture'])
             .read({
-              onSuccess: tag => resolve(tag),
-              onError: () => resolve(getFallbackTagData(zipEntry.name))
+              onSuccess: (tag) => resolve(tag),
+              onError: () => resolve(getFallbackTagData(zipEntry.name)),
             });
         })
     )
     .catch(() => Promise.reject(new Error('Unable to parse tag data')));
 };
 
-const loadVideo = function(zipEntry) {
+const loadVideo = function (zipEntry) {
   return zipEntry
     .async('uint8array')
     .catch(() => Promise.reject(new Error(`Unable to load the video file`)));
 };
 
-const getKaraokeFiles = function(zipFile) {
-  const entries = zipFile.filter(relPath => relPath.endsWith('.cdg') || relPath.endsWith('.mp3'));
+const getKaraokeFiles = function (zipFile) {
+  const entries = zipFile.filter((relPath) => relPath.endsWith('.cdg') || relPath.endsWith('.mp3'));
   if (entries.length === 2) {
     return Promise.resolve(entries);
   }
   return Promise.reject(`The file is not a karaoke .zip file`);
 };
 
-const processZip = function(entries) {
-  const audio = entries.filter(entry => entry.name.endsWith('.mp3'));
-  const video = entries.filter(entry => entry.name.endsWith('.cdg'));
+const processZip = function (entries) {
+  const audio = entries.filter((entry) => entry.name.endsWith('.mp3'));
+  const video = entries.filter((entry) => entry.name.endsWith('.cdg'));
   const process = [];
   if (audio.length && video.length) {
     process.push(loadAudio(audio[0]));
     process.push(loadVideo(video[0]));
     process.push(getTagData(audio[0]));
-    return Promise.all(process).catch(() =>
-      Promise.reject(new Error(`Processing audio and video failed`))
-    );
+    return Promise.all(process).catch((err) => Promise.reject(err));
   }
   const errors = [];
   if (!audio.length) {
@@ -94,16 +92,16 @@ const processZip = function(entries) {
 export default class CDGFileLoader {
   static loadZipFile(filePath) {
     return getDataFile(filePath)
-      .then(fileBuffer => loadZipBuffer(fileBuffer))
-      .then(zipFile => getKaraokeFiles(zipFile))
-      .then(entries => processZip(entries))
-      .catch(error => Promise.reject(error));
+      .then((fileBuffer) => loadZipBuffer(fileBuffer))
+      .then((zipFile) => getKaraokeFiles(zipFile))
+      .then((entries) => processZip(entries))
+      .catch((error) => Promise.reject(error));
   }
 
   static loadFileBuffer(fileBuffer) {
     return loadZipBuffer(fileBuffer)
-      .then(zipFile => getKaraokeFiles(zipFile))
-      .then(entries => processZip(entries))
-      .catch(err => Promise.reject(err));
+      .then((zipFile) => getKaraokeFiles(zipFile))
+      .then((entries) => processZip(entries))
+      .catch((err) => Promise.reject(err));
   }
 }
